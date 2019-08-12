858973787bf050b57d6d5d666ad5ac7281a45609
huberysun <sunlixiang@csvw.com>
2019-08-12 15:14:51 +0800

    修改：使用NavLink替换自定义组件TabItemLink

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import { Icon } from 'antd-mobile';
-import classnames from 'classnames';
 import './index.css';
 
 interface RouteConfig {
@@ -20,27 +19,27 @@ interface TabItemLinkProps {
   to: string;
 }
 
-/**
- * 导航tab组件
- * @param param0 
- */
-function TabItemLink({icon, title, to}: TabItemLinkProps) {
-  return (
-    <Route
-      exact
-      path={to}
-      children={({ match }) => {
-        const cls = classnames('tab-link', {'tab-link-selected': match});
-        return (
-          <Link className={cls} to={to}>
-            <Icon type={icon} size="md"></Icon>
-            <span>{title}</span>
-          </Link>
-        );
-      }}
-    ></Route>
-  );
-}
+// /**
+//  * 导航tab组件
+//  * @param param0 
+//  */
+// function TabItemLink({icon, title, to}: TabItemLinkProps) {
+//   return (
+//     <Route
+//       exact
+//       path={to}
+//       children={({ match }) => {
+//         const cls = classnames('tab-link', {'tab-link-selected': match});
+//         return (
+//           <Link className={cls} to={to}>
+//             <Icon type={icon} size="md"></Icon>
+//             <span>{title}</span>
+//           </Link>
+//         );
+//       }}
+//     ></Route>
+//   );
+// }
 
 const tabBarParams = [
   { icon: 'icon-ditu', title: '地图', to: '/home/map'},
@@ -57,7 +56,19 @@ export class Home extends React.Component<HomeProps, any> {
       <div>
         <div className="tab-bar">
           {
-            tabBarParams.map(item => <TabItemLink key={item.to} {...item}></TabItemLink>)
+            // tabBarParams.map(item => <TabItemLink key={item.to} {...item}></TabItemLink>)
+            tabBarParams.map(item => {
+              return (<NavLink
+                key={item.to}
+                to={item.to}
+                className="tab-link"
+                activeClassName="tab-link-selected"
+                exact>
+                  <Icon type={item.icon} size="md"></Icon>
+                  <span>{item.title}</span>
+                 </NavLink>
+              );
+            })
           }
         </div>
         {
